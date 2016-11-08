5f66f5a90a12dac76d932fb064ca08f09fc4f3db
Donghao Ren <donghao.ren@gmail.com>
2016-11-08 11:05:46 -0800

    Updated code.

--- stardust/stardust.bundle.js
+++ stardust/stardust.bundle.js
@@ -1,9 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 if(typeof(window) != "undefined") {
-    window.stardust = {
-        core: require("stardust-core"),
-        webgl: require("stardust-webgl")
-    };
+    window.Stardust = require("stardust-core");
+    window.StardustWebGL = require("stardust-webgl");
 }
 },{"stardust-core":22,"stardust-webgl":29}],2:[function(require,module,exports){
 // binding.js:
