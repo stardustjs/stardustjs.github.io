cf7db47bf82732c1d0926621fbfa0318c6af6a9d
Donghao Ren <donghao.ren@gmail.com>
2016-11-28 21:41:33 -0800

    Updated.

---
+++ examples/daily-activities/data.js
@@ -0,0 +1,339 @@
+
+  var activities = {
+    '18': [
+        ["food",0.5,0,"Drinking"],
+        ["sleep",7.5,0.5,"Sleep"],
+        ["food",1.5,8,"Grapefruit, cereal, banana, tea"],
+        ["creative",3.5,9.5,"He would pick up from where he left off the previous evening"],
+        ["food",3.5,13,"Lunch at the pub with a drink before and a coffee after"],
+        ["sleep",0.5,16.5,"Nap in his favorite armchair"],
+        ["creative",2,17,"Main work"],
+        ["food",4,19,"Dinner with family, drinking scotch and water whilst settling down to his favorite television programs"],
+        ["food",1,23,"Final scotch and water"]
+      ],
+     '7': [
+        ["sleep",5.5,0,"Sleep"],
+        ["other",0.5,5.5,"Getting dressed"],
+        ["food",0.5,6,"Coffee with husband"],
+        ["other",0.5,6.5,"Commute to hotel room she rented for work"],
+        ["creative",8,7,"Writing in hotel room, the more anonymous the better"],
+        ["food",4.5,15,"Shower, cook, have dinner with husband"],
+        ["creative",0.5,19.5,"Reading work to husband"],
+        ["food",2,20,"Time with husband"],
+        ["sleep",2,22,"Sleep"]
+      ],
+      '8': [
+        ["sleep",5.5,0,"Sleep"],
+        ["food",0.5,5.5,"Coffee with a crossword"],
+        ["creative",5,6,"Worked with a dose of Benzedrine, a daily routine he kept up for 20 years"],
+        ["food",0.5,11,"Lunch"],
+        ["creative",6.5,11.5,"Main work"],
+        ["food",1.5,18,"Strong vodka martinis"],
+        ["food",2.5,19.5,"Dinner with friends"],
+        ["sleep",2,22,"Used sedative to go to sleep"]
+      ],
+      '0': [
+        ["sleep",1,0,"Sleep"],
+        ["creative",7,1,"Wore monkish robe and Moroccan slippers for his priest-like work"],
+        ["sleep",1.5,8,"Nap"],
+        ["creative",6,9.5,"Had a cup of black coffee before starting work"],
+        ["food",2.5,15.5,"Received friends, took a bath, went out for dinner"],
+        ["sleep",6,18,"Sleep"]
+      ],
+      '9': [
+        ["sleep",6,0,"Sleep"],
+        ["food",0.5,6,"Just a coffee, 60 beans per cup "],
+        ["creative",8,6.5,"Worked on compositions at his desk, taking the occasional break to walk outdoors, which aided his creativity"],
+        ["food",1,14.5,"Dinner with wine"],
+        ["exercise",1,15.5,"Long walk"],
+        ["food",4,16.5,"Tavern to eat and to read a newspaper"],
+        ["food",1,20.5,"Supper"],
+        ["sleep",2.5,21.5,"Sleep"]
+      ],
+      '10': [
+        ["sleep",6,0,"Sleep"],
+        ["food",1,6,"Praying, having coffee"],
+        ["food",2,7,"Church"],
+        ["creative",3,9,"Work"],
+        ["food",9,12,"Hobbies, guests, painting, taking care of birds"],
+        ["sleep",3,21,"Sleep"]
+      ],
+      '11': [
+        ["sleep",6,0,"Sleep"],
+        ["exercise",0.5,6,"Preparing mind and body for the day ahead"],
+        ["food",2.5,6.5,"Breakfast with wife"],
+        ["creative",5.5,9,"Painting, drawing, writing"],
+        ["admin",3,14.5,"Office hours (getting others to work on his ideas)"],
+        ["food",5.5,17.5,"Time at home"],
+        ["sleep",1,23,"Sleep"]
+      ],
+      '14': [
+        ["sleep",7,0,"Sleep"],
+        ["exercise",0.5,7,"Walk"],
+        ["food",0.5,7.5,"Breakfast"],
+        ["creative",1.5,8,"Focused work"],
+        ["admin",1,9.5,"Reading mail"],
+        ["creative",1.5,10.5,"Work"],
+        ["exercise",0.5,12,"Walking the dog"],
+        ["food",0.5,12.5,"Lunch"],
+        ["admin",1,13,"Reading newspapers"],
+        ["admin",1,14,"Writing letters"],
+        ["sleep",1,15,"Nap"],
+        ["exercise",0.5,16,"Walk"],
+        ["admin",1,16.5,"Admin bits and bobs"],
+        ["food",0.5,17.5,"Idleness"],
+        ["food",1,18,"Reading for pleasure"],
+        ["food",1,19,"Tea with egg"],
+        ["food",1,20,"Playing backgammon"],
+        ["admin",1,21,"Reading science books"],
+        ["creative",2,22,"Lying in bed, solving problems"]
+      ],
+      '15': [
+        ["sleep",7,0,"Sleep"],
+        ["other",1,7,"Waking up"],
+        ["food",1,8,"Breakfast"],
+        ["creative",5,9,"Writing in absolute quiet"],
+        ["exercise",3,14,"Walking around London"],
+        ["food",1,17,"Gap time"],
+        ["food",1,18,"Dinner"],
+        ["food",5,19,"Time with friends and family"]
+      ],
+      '23': [
+        ["creative",3,0,"Writing"],
+        ["sleep",7,3,"Sleep"],
+        ["admin",0.5,10,"Newspapers, mail"],
+        ["other",0.5,10.5,"Taking a bath"],
+        ["food",1,11,"Light breakfast, typically eggs, vegetables, cheese or fruit and a cup of cold chocolate"],
+        ["exercise",1,12,"Outdoor stroll with family"],
+        ["admin",1,13,"Teaching history"],
+        ["creative",5,14,"Creative work, mostly reading"],
+        ["food",1,19,"Dinner with family"],
+        ["food",1.5,20,"Time with family, talking to mother"],
+        ["creative",2.5,21.5,"Creative work"]
+      ],
+      '4': [
+        ["sleep",5,0,"Sleep"],
+        ["food",3,5,"Wash, make a resolution, have breakfast"],
+        ["creative",4,8,"Work"],
+        ["food",2,12,"Reading, life admin, lunch"],
+        ["creative",4,14,"Work"],
+        ["food",4,18,"Supper, music, putting things in order, socialising"],
+        ["sleep",2,22,"Sleep"]
+      ],
+      '16': [
+        ["creative",1,0,"Writing"],
+        ["sleep",6,1,"Sleep"],
+        ["food",1,7,"Breakfast and trimming beard"],
+        ["creative",4,8,"Treating patients"],
+        ["food",1,12,"Gap time"],
+        ["food",1,13,"Lunch"],
+        ["exercise",1,14,"Speedy walk"],
+        ["creative",6,15,"Consulting and treating patients"],
+        ["food",1.5,21,"Supper and card games"],
+        ["creative",1.5,22.5,"Reading and writing"]
+      ],
+      '12': [
+        ["sleep",6,0,"Sleep"],
+        ["food",0.5,6,"Coffee and two raw eggs"],
+        ["creative",4.5,6.5,"Writing"],
+        ["other",1,11,"Ice bath on the roof"],
+        ["food",1,12,"Guests and lunch"],
+        ["exercise",2,13,"Long, strenuous exercise"],
+        ["other",1,15,"Visit to the barber"],
+        ["food",2,16,"Date"],
+        ["creative",2,18,"Writing"],
+        ["food",2,20,"Dinner, playing cards, going out"],
+        ["sleep",2,22,"Sleep"]
+      ],
+      '22': [
+        ["creative",6,0,"Writing"],
+        ["sleep",2.5,6,"Sleeping after struggling to fall asleep"],
+        ["admin",6,8.5,"Work at the worker's accident insurance institute"],
+        ["food",1,14.5,"Lunch"],
+        ["sleep",4,15.5,"Catching up on sleep"],
+        ["exercise",1.5,19.5,"Exercises next to the window, then a walk"],
+        ["food",2,21,"Dinner with family"],
+        ["creative",1,23,"Writing"]
+      ],
+      '5': [
+        ["sleep",5,0,"Sleep"],
+        ["food",1,5,"Tea and pipe"],
+        ["creative",1,6,"Writing"],
+        ["admin",4,7,"Lecturing at the university"],
+        ["food",4,11,"Meat and wine at the pub for lunch, main meal of the day"],
+        ["exercise",1,15,"Walk"],
+        ["creative",3,16,"Working with Joseph Green"],
+        ["food",3,19,"Reading"],
+        ["sleep",2,22,"Sleep"]
+      ],
+      '19': [
+        ["sleep",8,0,"Sleep"],
+        ["other",1,8,"Taking a bath"],
+        ["creative",3,9,"Writing"],
+        ["food",0.5,12,"Lunch"],
+        ["creative",3.5,12.5,"Reading"],
+        ["sleep",1,16,"Nap"],
+        ["food",0.5,17,"Tea with family"],
+        ["admin",1.5,17.5,"Reading newspapers, mail"],
+        ["exercise",0.5,19,"Walk"],
+        ["food",4.5,19.5,"Time with family, guests, listening to music"]
+      ],
+      '1': [
+        ["sleep",4,0,"Sleep"],
+        ["other",1,4,"Meditation"],
+        ["food",2,5,"Listening to aide reading bible"],
+        ["creative",3,7,"Dictating to aide"],
+        ["creative",2,10,"Listening to aide read back"],
+        ["food",1,12,"Lunch"],
+        ["exercise",3,13,"Walk up and down his garden"],
+        ["food",5,16,"Having guests over, supper, smoking a pipe"],
+        ["sleep",3,21,"Sleep"]
+      ],
+      '13': [
+        ["creative",1,0,"Composing"],
+        ["sleep",5,1,"Sleep"],
+        ["other",1,6,"Getting dressed"],
+        ["creative",2,7,"Composing"],
+        ["admin",4,9,"Giving music lessons"],
+        ["food",4,13,"Lunch with friends"],
+        ["creative",4,17,"Composing or giving concerts"],
+        ["food",2,21,"Time with friends"],
+        ["creative",1,23,"Composing"]
+      ],
+      '2': [
+        ["sleep",4,0,"Sleep"],
+        ["creative",8,4,"Writing"],
+        ["exercise",1,12,"Walking and swimming"],
+        ["food",8,13,"Doing housework, admin and spending time with family"],
+        ["sleep",3,21,"Sleep"]
+      ],
+      '17': [
+        ["sleep",1,0,"Trying to fall sleep"],
+        ["sleep",6.5,1,"Sleep"],
+        ["food",1,7.5,"Thinking in bed"],
+        ["food",1.5,8.5,"Shave, breakfast, meditation, bath"],
+        ["creative",1,10,"Writing"],
+        ["exercise",0.5,11,"Walk"],
+        ["creative",1.5,11.5,"Work"],
+        ["food",1,13,"Lunch"],
+        ["creative",4.5,14,"Writing"],
+        ["exercise",0.5,18.5,"Walk"],
+        ["food",2,19,"Time with family"],
+        ["creative",2.5,21,"Reading"],
+        ["sleep",0.5,23.5,"Trying to sleep"]
+      ],
+      '24': [
+        ["creative",2.5,0,"Painting"],
+        ["sleep",8.5,2.5,"Sleep"],
+        ["food",4,11,"Breakfast, seeing friends"],
+        ["creative",7.5,15,"Painting"],
+        ["food",1,22.5,"Dinner"],
+        ["creative",0.5,23.5,"Painting"]
+      ],
+      '20': [
+        ["sleep",8,0,"Sleep"],
+        ["food",1.5,8,"Breakfast of 3 eggs, tea, and jam; bath"],
+        ["exercise",0.5,9.5,"Walk"],
+        ["creative",3,10,"Composing"],
+        ["food",1,13,"Lunch with friends"],
+        ["food",1,14,"Reading or playing games"],
+        ["creative",1,15,"Composing"],
+        ["food",1,16,"Dinner and tea"],
+        ["exercise",1,17,"Walk"],
+        ["admin",1,18,"Writing letters"],
+        ["food",2.5,19,"Dinner and socialising"],
+        ["food",0.5,21.5,"Reading for pleasure"],
+        ["sleep",2,22,"Sleep"]
+      ],
+      '25': [
+        ["food",1,0,"Up at night"],
+        ["food",2,1,"Listening to music, drinking, smoking"],
+        ["sleep",9,3,"Sleep"],
+        ["food",1.5,12,"Reading and thinking in bed"],
+        ["food",1,13.5,"Lunch"],
+        ["admin",1.5,14.5,"Errands, mail"],
+        ["creative",4,16,"Writing"],
+        ["food",4,20,"Dinner with family, then listening to music, drinking, smoking"]
+      ],
+      '21': [
+        ["sleep",8,0,"Sleep"],
+        ["food",1,8,"Tea, smoking, reading bible and philosophy"],
+        ["exercise",0.5,9,"Walk"],
+        ["admin",0.5,9.5,"Mail"],
+        ["creative",2,10,"Composing"],
+        ["food",1,12,"Lunch"],
+        ["exercise",2,13,"Long walk"],
+        ["food",2,15,"Tea, reading newspapers"],
+        ["creative",2,17,"Composing"],
+        ["other",1,19,"Gap time"],
+        ["food",4,20,"Dinner, having guests, socialising"]
+      ],
+      '3': [
+        ["sleep",4,0,"Sleep"],
+        ["creative",8,4,"Working in bed, dictating his thoughts to a secretary"],
+        ["other",0.5,12,"Getting dressed"],
+        ["creative",1.5,12.5,"Working"],
+        ["admin",2,14,"Surveying the estate with secretary"],
+        ["creative",4,16,"Working"],
+        ["food",1.5,20,"Dinner with family"],
+        ["creative",2.5,21.5,"Working"]
+      ],
+      '6': [
+        ["sleep",5.5,0,"Sleep"],
+        ["creative",2.5,5.5,"Writing"],
+        ["food",0.5,8,"Breakfast"],
+        ["creative",1.5,8.5,"Writing"],
+        ["exercise",1.5,10,"Walk, swim"],
+        ["food",1,11.5,"Lunch"],
+        ["admin",5,12.5,"Teaching at school"],
+        ["food",4.5,17.5,"Drinking scotch, making supper, listenting to jazz"],
+        ["sleep",2,22,"Sleep"]
+    ]
+  };
+  var about = [
+  ["Honoré de Balzac","Writer","Hailed as a founder of realism, he shook the 19th century literary scene with his unflinching take on Parisian society."],
+  ["John Milton","Poet","Milton’s poem ‘Paradise Lost’ is an epic in the true sense of the word – originally published as ten books and over ten thousand lines of verse. Writing in the 17th century, during a time of religious upheaval, he passionately defended the right to free speech."],
+  ["Haruki Murakami","Writer","A contemporary Japanese author, Murakami penned works including ‘Norwegian Wood’ and ‘Kafka on the Shore’. His magic realist style has earned him the Franz Kafka Prize, and other awards."],
+  ["Voltaire","Writer","A French Enlightenment writer, outspoken critic of the Catholic Church and defender of free speech, Voltaire applied his witty approach to all sorts of literary forms including novels, poems and essays. "],
+  ["Benjamin Franklin","Politician","One of the founding fathers of the United States, Franklin earned the title of the ‘first American’. Through his writing and campaigning for colonial unity, he was a defining voice in the shaping of American society."],
+  ["Immanuel Kant","Philosopher","A German philosopher who continues to have a profound influence on contemporary thought. Among his achievements, he established a firm basis for factual knowledge, paving the way for the scientific method."],
+  ["Kurt Vonnegut","Writer","Described as a counterculture’s novelist, Vonnegut wrote in a satirical style that embodied the more cynical views of American society. His works like ‘Cat’s Cradle’ are praised for their blending of science fiction with gallows humor."],
+  ["Maya Angelou","Writer","Poet and author, Angelou represented a strong voice of the Civil Rights movement. Her novel ‘I Know Why the Caged Bird Sings’ brought her international acclaim for its frank exploration of race and identity. "],
+  ["Wystan Hugh Auden","Poet","Auden was an English-born poet who wrote about religion, politics and love. His 400-odd poems ranged enormously in style, from ballads and haikus to obscure modern forms."],
+  ["Ludwig Van Beethoven ","Composer","A revolutionary in the music world, Beethoven broke with convention to compose highly original works, continuing to write even after losing his hearing."],
+  ["Mary Flannery O'Connor","Writer","Known as one of America’s most important fiction writers, O’Connor developed a Southern Gothic style. Her works include ‘The Violent Bear it Away’ which explores themes of violence and spiritual awakening."],
+  ["Le Corbusier","Architect","A Swiss-French architect who enraged the establishment with his bold modern designs. Famous works include Unité d'Habitation in Marseilles, and Notre Dame du Haut in Ronchamp."],
+  ["Victor Hugo","Writer","This French writer brought the world ‘Les Misérables’ and ‘The Hunchback of Notre-Dame’, among other novels. He was one of the few Romanticists to have achieved popularity during his lifetime. "],
+  ["Wolfgang Amadeus Mozart","Composer","A composer from the age of 5, Mozart worked prolifically throughout his life.  He created such masterpieces as ‘Symphony No. 40’ and the opera ‘Don Giovanni’. "],
+  ["Charles Darwin","Scientist","In ‘The Origin of the Species’, Darwin established his theory of evolution, along with his place in history as a revolutionary thinker."],
+  ["Charles Dickens","Writer","Arguably the most famous novelist of the Victorian period, Dickens is known for his vivid characters, as found in novels like ‘Great Expectations’ and ‘The Christmas Carol’. "],
+  ["Sigmund Freud","Psychoanalyst","Freud both shocked and intrigued European society with his theories of the human psyche and sexual urges. Writings like ‘The Interpretation of Dreams’ are still key texts in academic courses around the world. "],
+  ["Vladimir Nabokov ","Writer","A Russian American novelist who wrote the daring ‘Lolita’. The book is among the most controversial yet enduring works of 20th century literature. "],
+  ["Kingsley Amis","Writer","This London-born writer was best known for his comedic novels. ‘Lucky Jim’ was his first and most famous. "],
+  ["Thomas Mann","Writer","Mann’s bold use of symbolism and irony made him one of Germany’s most significant 20th century writers. He drew on influences from Nietzsche to Goethe to express the psychology of the artist and the intellectual. "],
+  ["Richard Strauss","Composer","A leading composer of the German Romantic movement, Strauss is most famed for his operas, including the modern, dissonant work ‘Salome’, which produced a highly passionate reaction from the audience who cheered for 38 curtain calls. "],
+  ["Pyotr Ilyich Tchaikovsky","Composer","Russia’s most famous composer, Tchaikovsky created works throughout his life, including ‘The Nutcracker’ and ‘The Sleeping Beauty’. Piano lessons began when he was just 5 years old and prompted a life-long fascination with music."],
+  ["Franz Kafka","Writer","The author of ‘Metamorphosis’, Prague-born Kafka helped shape the course of modern literature, exploring existential themes of alienation and fear."],
+  ["Gustave Flaubert","Writer","Author of the controversial ‘Madame Bovary’, Flaubert was charged with immorality by the government. Luckily, he was acquitted and continued to write his candid portraits of life in French society."],
+  ["Pablo Picasso","Painter","Potentially the most important painter of the 20th century, Picasso’s long career spanned many styles of artwork, including the angular lines of Cubism, for which he is most well-known."],
+  ["William Styron","Writer","Author of ‘Sophie’s Choice’, Styron was an American novelist considered to be one of the true original writers of his generation. His work often caused controversy for exploring challenging moral themes and morbid subject matter. "]
+];
+
+let data  = about.map((d, i) => {
+    var artist = {
+        name: d[0],
+        type: d[1],
+        description: d[2],
+        activities: activities[i].map((d) => {
+            return {
+                type: d[0],
+                duration: d[1],
+                start: d[2],
+                description: d[3]
+            };
+        })
+    };
+    return artist;
+});
+
+require("fs").writeFileSync("data.json", JSON.stringify(data, null, 2), "utf-8");
